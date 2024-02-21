23e42ec6d265cdc1b1e908c912a59d094fb1cf3f
Inno Melkonjan <monkeytoffy@gmail.com>
2024-02-21 18:16:16 +0100

    half navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../App.css'
+
+export const Navbar = () => {
+  return (
+    <header>
+    <div className="logo-div">
+        <img src="" alt="logo" />
+    </div>
+    <div className="header-navbar">
+        <div>
+          <p>texblanlbalba</p>
+          
+        </div>
+        <div>
+
+        </div>
+    </div>
+</header>
+  )
+}
+
+export default Navbar
